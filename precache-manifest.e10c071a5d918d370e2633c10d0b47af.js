14f1a76e7ee7202c8621e4f3171ff560765fba8b
suraj kamal.shaik <surajsheru@gmail.com>
2020-06-15 16:06:09 +0530

    Updates

---
+++ precache-manifest.e10c071a5d918d370e2633c10d0b47af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88645ca0bcc6a137738a16cece344e9",
+    "url": "/peopleten/index.html"
+  },
+  {
+    "revision": "0355b4bcf2136ab4a637",
+    "url": "/peopleten/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ec6023a74b730160ec96",
+    "url": "/peopleten/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0355b4bcf2136ab4a637",
+    "url": "/peopleten/static/js/2.8c3fae2e.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/peopleten/static/js/2.8c3fae2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6023a74b730160ec96",
+    "url": "/peopleten/static/js/main.d7916f1b.chunk.js"
+  },
+  {
+    "revision": "47cba9427243bdf2b63d",
+    "url": "/peopleten/static/js/runtime-main.4671299e.js"
+  },
+  {
+    "revision": "c2eee7e2fc67259a8a8384c2e97b75b9",
+    "url": "/peopleten/static/media/T1.c2eee7e2.jpg"
+  },
+  {
+    "revision": "88056c83d2644ff92b9c2718a6f3b698",
+    "url": "/peopleten/static/media/T3.88056c83.jpg"
+  },
+  {
+    "revision": "19308b1f2b87d2e410be4131d67dfb00",
+    "url": "/peopleten/static/media/T4.19308b1f.jpg"
+  }
+]);
